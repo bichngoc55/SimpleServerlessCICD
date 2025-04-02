5afec0802cdc87be1d0634c17dd9e65813dde27a
Gấu <115764988+bichngoc55@users.noreply.github.com>
2025-04-02 13:33:02 +0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 exports.lambdaHandler = async (event, context) => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Hello World Omega33333!'
-            })
-        };
-    } catch (err) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'Internal Server Error',
-                error: err
-            })
-        };
-    }
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Hello World Omega33333!",
+      }),
+      //  i love tiktok
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Internal Server Error",
+        error: err,
+      }),
+    };
+  }
 };
